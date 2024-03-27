7104200342a7c475363684dc6285f21fdfe2d280
Guy Baudouin <guythoun@gmail.com>
2024-03-27 10:06:12 +0100

    clean metadatas

--- gameinfos.inc.php
+++ gameinfos.inc.php
@@ -10,19 +10,10 @@
 
 */
 
-$gameinfos = array( 
+$gameinfos = [
 
 // Name of the game in English (will serve as the basis for translation) 
 'game_name' => "Sobek 2 players",
-
-// Game designer (or game designers, separated by commas)
-'designer' => 'Bruno Cathala, Sébastien Pauchon',       
-
-// Game artist (or game artists, separated by commas)
-'artist' => 'Xavier Gueniffey Durin',         
-
-// Year of FIRST publication of this game. Can be negative.
-'year' => 2021,                 
 
 // Game publisher (use empty string if there is no publisher)
 'publisher' => 'Catch Up Games',                     
@@ -38,7 +29,7 @@ $gameinfos = array(
 
 
 // Players configuration that can be played (ex: 2 to 4 players)
-'players' => array( 2 ),    
+'players' => [2],    
 
 // Suggest players to play with this number of players. Must be null if there is no such advice, or if there is only one possible player configuration.
 'suggest_player_number' => null,
@@ -84,19 +75,7 @@ $gameinfos = array(
 // Language dependency. If false or not set, there is no language dependency. If true, all players at the table must speak the same language.
 // If an array of shortcode languages such as array( 1 => 'en', 2 => 'fr', 3 => 'it' ) then all players at the table must speak the same language, and this language must be one of the listed languages.
 // NB: the default will be the first language in this list spoken by the player, so you should list them by popularity/preference.
-'language_dependency' => false,
-
-// Complexity of the game, from 0 (extremely simple) to 5 (extremely complex)
-'complexity' => 2,    
-
-// Luck of the game, from 0 (absolutely no luck in this game) to 5 (totally luck driven)
-'luck' => 3,    
-
-// Strategy of the game, from 0 (no strategy can be setup) to 5 (totally based on strategy)
-'strategy' => 3,    
-
-// Diplomacy of the game, from 0 (no interaction in this game) to 5 (totally based on interaction and discussion between players)
-'diplomacy' => 2,    
+'language_dependency' => false, 
 
 // Colors attributed to players
 'player_colors' => array( "ff0000", "008000", "0000ff", "ffa500", "773300" ),
@@ -126,30 +105,6 @@ $gameinfos = array(
     'max' => null
 ),
 
-// Game presentation
-// Short game presentation text that will appear on the game description page, structured as an array of paragraphs.
-// Each paragraph must be wrapped with totranslate() for translation and should not contain html (plain text without formatting).
-// A good length for this text is between 100 and 150 words (about 6 to 9 lines on a standard display)
-'presentation' => array(
-    totranslate("In Sobek: 2 Players, both opponents collect and sell Goods to earn the most Victory Points."),
-    totranslate("On their turn, the player chooses among 3 options: take a tile on the Market, sell a set of Goods or play a Character."),
-    totranslate("When the player takes a tile, they select one on the Ankh pawn’s line and add it to their hand."),
-    totranslate("They then place the Ankh pawn in the line that is indicated by the tile they just took, which determines the choices available to their opponent."),
-    totranslate("When they have enough in hand, a player can sell a set of at least 3 tiles of the same type."),
-    totranslate("These tiles will earn them Victory Points at the end of the game, depending on both their quantity and the amount of Scarabs that they feature."),
-    totranslate("A player can also use a Character to benefit from its powerful special ability."),
-    totranslate("Planning ahead and analysing which opportunities you offer to you opponent on their next move is key to playing the best moves and winning the game."),
-),
-
-// Games categories
-//  You can attribute a maximum of FIVE "tags" for your game.
-//  Each tag has a specific ID (ex: 22 for the category "Prototype", 101 for the tag "Science-fiction theme game")
-//  Please see the "Game meta information" entry in the BGA Studio documentation for a full list of available tags:
-//  http://en.doc.boardgamearena.com/Game_meta-information:_gameinfos.inc.php
-//  IMPORTANT: this list should be ORDERED, with the most important tag first.
-//  IMPORTANT: it is mandatory that the FIRST tag is 1, 2, 3 and 4 (= game category)
-'tags' => array( 2, 11, 30, 210 ),
-
 
 //////// BGA SANDBOX ONLY PARAMETERS (DO NOT MODIFY)
 
@@ -160,4 +115,4 @@ $gameinfos = array(
 'turnControl' => 'simple'
 
 ////////
-);
+];
